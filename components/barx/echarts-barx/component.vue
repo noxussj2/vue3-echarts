46d75025735176aa463401c736b650688faa847c
noxussj2 <2444783832@qq.com>
2024-01-18 09:54:42 +0800

    完善纵向柱状图 barx

--- components/barx/echarts-barx/component.vue
+++ components/barx/echarts-barx/component.vue
@@ -9,19 +9,19 @@ import render from './render'
 const props = defineProps({
 
     /**
-     * 高度
-     */
-    height: {
-        type: String,
-        default: '100%'
-    },
-
-    /**
      * 用户配置项（继承已有配置，非必要时候勿用）
      */
     opt: {
         type: Object,
         default: () => ({})
+    },
+
+    /**
+     * 高度
+     */
+    height: {
+        type: String,
+        default: '100%'
     },
 
     /**
@@ -54,6 +54,46 @@ const props = defineProps({
     barWidth: {
         type: Number,
         default: 15
+    },
+
+    /**
+     * 是否堆叠
+     */
+    stack: {
+        type: Boolean,
+        default: false
+    },
+
+    /**
+     * 圆角
+     */
+    radius: {
+        type: Number,
+        default: 0
+    },
+
+    /**
+     * 是否独立颜色
+     */
+    singleColor: {
+        type: Boolean,
+        default: false
+    },
+
+    /**
+     * 渐变颜色
+     */
+    gradientColor: {
+        type: Array,
+        default: () => []
+    },
+
+    /**
+     * 是否显示背景
+     */
+    showBackground: {
+        type: Boolean,
+        default: false
     }
 })
 
@@ -63,7 +103,18 @@ onMounted(() => {
     watch(
         () => props.data,
         () => {
-            render({ $dom: echarts, $opt: props.opt, $data: props.data, $seriesColor: props.color, $barWidth: props.barWidth })
+            render({
+                $dom: echarts,
+                $opt: props.opt,
+                $data: props.data,
+                $seriesColor: props.color,
+                $barWidth: props.barWidth,
+                $stack: props.stack,
+                $radius: props.radius,
+                $singleColor: props.singleColor,
+                $gradientColor: props.gradientColor,
+                $showBackground: props.showBackground
+            })
         },
         {
             deep: true,
--- components/barx/echarts-barx/render.ts
+++ components/barx/echarts-barx/render.ts
@@ -2,17 +2,69 @@ import _echarts from '../../../utils/echarts-register'
 import { extens } from '../../../core/echarts-extens'
 import { useStyle } from '../../../styles'
 
-export default async ({ $dom, $opt, $data, $seriesColor, $barWidth }: any) => {
+export default async ({ $dom, $opt, $data, $seriesColor, $barWidth, $stack, $radius, $singleColor, $gradientColor, $showBackground }: any) => {
     const { $color, $grid, $tooltip, $vertical, $legend } = useStyle()
 
-    const series: any = []
+    /**
+     * 过滤主题色
+     */
+    const color = $seriesColor || $color.theme
 
-    $data.series.forEach((item: any) => {
+    /**
+     * 数据处理
+     */
+    const series: any = []
+    $data.series.forEach((item: any, index: number) => {
+        const data = []
+
+        item.data.forEach((x: any, i: number) => {
+
+            /**
+             * 常规颜色
+             */
+            let _color = $singleColor ? color[i] : color[index]
+
+            /**
+             * 渐变颜色
+             */
+            if ($gradientColor.length === 2) {
+                _color = {
+                    type: 'linear',
+                    x: 0,
+                    y: 0,
+                    x2: 0,
+                    y2: 1,
+                    colorStops: [
+                        {
+                            offset: 0,
+                            color: $gradientColor[0]
+                        },
+                        {
+                            offset: 1,
+                            color: $gradientColor[1]
+                        }
+                    ]
+                }
+            }
+
+            data.push({
+                value: x,
+                itemStyle: {
+                    color: _color
+                }
+            })
+        })
+
         series.push({
             type: item.type || 'bar',
             name: item.name,
-            data: item.data,
-            barWidth: $barWidth
+            data,
+            barWidth: $barWidth,
+            stack: $stack,
+            itemStyle: {
+                borderRadius: $radius
+            },
+            showBackground: $showBackground
         })
     })
 
@@ -20,7 +72,7 @@ export default async ({ $dom, $opt, $data, $seriesColor, $barWidth }: any) => {
      * 导出配置项
      */
     const options = {
-        color: $seriesColor || $color.theme,
+        color,
         grid: $grid,
         tooltip: Object.assign(
             {
